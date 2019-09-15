6d253abaf84eeb15423ada5c9318520f41d9a057
Matthew Hailwood <matthew@hailwood.nz>
2019-09-16 10:47:32 +1200

    Update MyEnumNoArguments.php

--- tests/fixtures/MyEnumNoArguments.php
+++ tests/fixtures/MyEnumNoArguments.php
@@ -11,9 +11,9 @@ use MadWeb\Enum\Enum;
  */
 final class MyEnumNoArguments extends Enum
 {
-    const __default = self::FOO;
+    public const __default = self::FOO;
 
-    const FOO = 0;
-    const BAR = 1;
-    const BAZ = 2;
+    public const FOO = 0;
+    public const BAR = 1;
+    public const BAZ = 2;
 }
